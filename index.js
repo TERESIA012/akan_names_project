d38206808806027082bf7cf73f4012e12c06cb4b
TERESIA012 <kingoriteresia@gmail.com>
2021-07-18 03:01:41 +0300

    modify js

--- index.js
+++ index.js
@@ -2,25 +2,25 @@
 function validate(){
     var date=document.getElementById("date").value;
     var month=document.getElementById("month").value;
-     var year=document.getElementById("year").value;
+    var year=document.getElementById("year").value;
 
 
-     if(date=="" || month=="" ||year==""){
+    if(date=="" || month=="" ||year==""){
          return false;
-     }else{
+    }else{
           var CC=year.slice(0,1);
           var YY=year.slice(2,3);
-          var dayOfTheWeek=( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(month+1)/10)) + date ) % 7;
+          var dayOfTheWeek=Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(month+1)/10)) + date ) % 7);
           return dayOfTheWeek;
  }
     
 }
-function gender(){
+function myGender(){
     var genderCheck=document.getElementsByName("gender");
     var female=document.getElementById("female");
     var male=document.getElementById("male");
 
-    if(!(genderCheck[0].checked ||genderCheck[1].checked)){
+    if(!(genderCheck[0].checked || genderCheck[1].checked)){
         return false;
     }else if(female.checked){
         genderCheck=female.value;
@@ -34,16 +34,25 @@ function gender(){
 
 
 function yourAkanName(){
-var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-var weekDays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    var weekDays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-if(gender ()==="male"){
-    alert(maleNames)
+    if(myGender()==="male"){
+    document.querySelector(".any").innerHTML="Your Akan Name is "+maleNames[validate()];
+    document.querySelector(".text").innerHTML="Your Akan Birth day is "+weekDays[validate()];
+    //alert("Hi Joe");
+    }else if(myGender()==="female"){
+    document.querySelector(".any").innerHTML="Your Akan Name is "+femaleNames[validate()];
+    document.querySelector(".text").innerHTML="Your Akan Birth day is "+weekDays[validate()];
+    }else{
+    document.querySelector(".any").innerHTML="Your Akan Name is "+femaleNames[validate()];
+    document.querySelector(".text").innerHTML="Your Akan Birth day is "+weekDays[validate()];
+
+    }
 }
 
 
-}
 
 
  
